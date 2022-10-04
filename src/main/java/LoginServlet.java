134d738d5f579aea2f3a764b9f49b81445233201
raakostOnCph <36969041+raakostOnCph@users.noreply.github.com>
2022-10-04 08:54:26 +0200

    laver tjeck for at alle felter er udfyldt ved opret

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -12,6 +12,9 @@ public class LoginServlet extends HttpServlet
 
         System.out.println("du ramte login servletten via Get");
 
+
+        request.getRequestDispatcher("WEB-INF/Bruger side.jsp").forward(request, response);
+
     }
 
     @Override
@@ -19,5 +22,31 @@ public class LoginServlet extends HttpServlet
     {
 
         System.out.println("du ramte login servletten via Post");
+
+
+        String opretNavn = request.getParameter("OpretNavn");
+        String kode1 = request.getParameter("kode1");
+        String kode2 = request.getParameter("kode2");
+
+        String besked ="";
+
+        if (opretNavn.equals("") || kode1.equals("") || kode2.equals("") ) {
+            besked = "huske alle felter skal udfyldes";
+
+            System.out.println("opret bruger ikke udfyldt korrekt");
+           // log("opret bruger ikke udfyldt korrekt");
+
+            request.setAttribute("besked", besked);
+            request.getRequestDispatcher("index.jsp").forward(request,response);
+
+
+        }
+
+
+
+        request.getRequestDispatcher("WEB-INF/Bruger side.jsp").forward(request, response);
+
+
+
     }
 }
---
+++ src/main/webapp/WEB-INF/Bruger side.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: nbh
+  Date: 04/10/2022
+  Time: 08.39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Bruger side</title>
+</head>
+<body>
+
+<h1> velkommen til din brugerside</h1>
+
+</body>
+</html>
